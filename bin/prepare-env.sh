f9547b513fcfd10301c072c383245982424f2aa1
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-01-25 16:37:40 +0100

    Added variable handling, but not sure if it works

--- bin/prepare-env.sh
+++ bin/prepare-env.sh
@@ -4,12 +4,18 @@ export FOUND_DOMAINS=`env | grep "DOMAIN" | cut -d_ -f1 | sort -u`
 echo "Following domains were found:"
 for FOUND_DOMAIN in $FOUND_DOMAINS
 do
-  echo $FOUND_DOMAIN
+  echo $FOUND_DOMAIN, ${FOUND_DOMAIN}_NAME
 done
-
-
-
 
 # Setting up site config for each domain
 echo "Setting up site config for each domain"
-
+for FOUND_DOMAIN in $FOUND_DOMAINS
+do
+  cp /etc/nginx/ssl-template.cfg /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  sed -i "s|%DOMAIN_NAME%|${{FOUND_DOMAIN}_NAME}||g" /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  sed -i "s|%SSL_CERT%|${{FOUND_DOMAIN}_SSL_CERT}||g" /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  sed -i "s|%SSL_KEY%|${{FOUND_DOMAIN}_SSL_KEY}||g" /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  sed -i "s|%LISTEN_PORT%|${{FOUND_DOMAIN}_LISTEN_PORT}||g" /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  sed -i "s|%LOG_FILE%|${{FOUND_DOMAIN}_LOG_FILE}||g" /etc/nginx/sites-enabled/${FOUND_DOMAIN}_NAME
+  echo "Config set up for " ${FOUND_DOMAIN}_NAME
+done
