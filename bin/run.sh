26d561fdce09cd79cabe5362375eb7ab068a95b9
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-02-08 17:08:36 +0100

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,4 @@ echo "Install bash and tools necessary later"
 apk add bash bash-doc bash-completion util-linux pciutils usbutils coreutils binutils findutils grep
 /prepare-env.sh && echo "Preparation finished"
 echo " finished preparation"
-#exec nginx -g "daemon off;"
+exec "$@"
