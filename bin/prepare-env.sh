531718e46b78463aa36e5db92185cb75b6a5a75a
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-02-08 17:56:42 +0100

    Update prepare-env.sh

--- bin/prepare-env.sh
+++ bin/prepare-env.sh
@@ -27,31 +27,31 @@ do
   TMP_LISTEN_PORT="${FOUND_DOMAIN}_LISTEN_PORT"
   TMP_LOG_FILE="${FOUND_DOMAIN}_LOG_FILE"
   
-  echo "For the domain ", ${!TMP_DOMAIN_NAME}
+  echo "For the domain " ${!TMP_DOMAIN_NAME}
   echo "the following config will be used:"
-  echo "SSL_CERT: ", ${!TMP_SSL_CERT}
-  echo "SSL_KEY: ", ${!TMP_SSL_KEY}
-  echo "LISTEN_PORT: ", ${!TMP_LISTEN_PORT}
-  echo "LOG_FILE: ", ${!TMP_LOG_FILE}
+  echo "SSL_CERT: " ${!TMP_SSL_CERT}
+  echo "SSL_KEY: " ${!TMP_SSL_KEY}
+  echo "LISTEN_PORT: " ${!TMP_LISTEN_PORT}
+  echo "LOG_FILE: " ${!TMP_LOG_FILE}
   
   cp /etc/nginx/ssl-template-part1.cfg /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  echo "    server_name ",  ${!TMP_DOMAIN_NAME}, ";"      >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo ""                                              >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "    ssl_certificate ",  ${!TMP_SSL_CERT}, ";"     >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME} 
-  echo "    ssl_certificate_key ",  ${!TMP_SSL_KEY}, ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "    server_name "  ${!TMP_DOMAIN_NAME} ";"      >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo ""                                               >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "    ssl_certificate "  ${!TMP_SSL_CERT} ";"     >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME} 
+  echo "    ssl_certificate_key "  ${!TMP_SSL_KEY} ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  cat /etc/nginx/ssl-template-part2.cfg                >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  cat /etc/nginx/ssl-template-part2.cfg                 >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  echo "    access_log ",  ${!TMP_LOG_FILE}, ";"          >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "    access_log "  ${!TMP_LOG_FILE} ";"          >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  cat /etc/nginx/ssl-template-part3.cfg                >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  cat /etc/nginx/ssl-template-part3.cfg                 >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  echo "        proxy_pass          http://localhost:",  ${!TMP_LISTEN_PORT}, ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "        proxy_read_timeout  90;"                                        >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo ""                                                                       >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "        proxy_redirect      http://localhost:",  ${!TMP_LISTEN_PORT}, " https://", ${!TMP_DOMAIN_NAME}, ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "      proxy_pass          http://localhost:"${!TMP_LISTEN_PORT} ";"    >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "      proxy_read_timeout  90;"                                         >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo ""                                                                      >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "      proxy_redirect      http://localhost:"${!TMP_LISTEN_PORT}" https://"${!TMP_DOMAIN_NAME} ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
   
-  echo "Config set up for ", ${!TMP_DOMAIN_NAME}
+  echo "Config set up for " ${!TMP_DOMAIN_NAME}
   echo ""
 done
