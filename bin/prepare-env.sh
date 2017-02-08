e1cdf9759c9d5b13c003b33828c8801895e53b71
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-02-08 19:08:43 +0100

    Changed to conf.d

--- bin/prepare-env.sh
+++ bin/prepare-env.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Removing default nginx site-enabled config and replace with data container"
+echo "Removing default nginx conf.d config and replace with data container"
 if [ -f /data ] ; then
-  echo "Removing sites-enabled"
-  rm -rf /etc/nginx/sites-enabled
+  echo "Removing conf.d"
+  rm -rf /etc/nginx/conf.d
 else
-  echo "Moving sites-enabled to data"
-  mv /etc/nginx/sites-enabled/* /data/
+  echo "Moving conf.d to data"
+  mv /etc/nginx/conf.d/* /data/
 fi
 
 echo "Linking data directory"
-ln -s /data /etc/nginx/sites-enabled
+ln -s /data /etc/nginx/conf.d
 
 # Found domains: cut gets only first field, sort delivers only unique entries
 export FOUND_DOMAINS=`env | grep -o "DOMAIN[0-9]*" | cut -d_ -f1 | sort -u`
@@ -49,27 +49,27 @@ do
   echo "LISTEN_PORT: " ${!TMP_LISTEN_PORT}
   echo "LOG_FILE: " ${!TMP_LOG_FILE}
   
-  cp /etc/nginx/ssl-template-part1.cfg /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  cp /etc/nginx/ssl-template-part1.cfg /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
-  echo "    server_name "  ${!TMP_DOMAIN_NAME} ";"      >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo ""                                               >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "    ssl_certificate "  ${!TMP_SSL_CERT} ";"     >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME} 
-  echo "    ssl_certificate_key "  ${!TMP_SSL_KEY} ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "    server_name "  ${!TMP_DOMAIN_NAME} ";"      >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo ""                                               >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "    ssl_certificate "  ${!TMP_SSL_CERT} ";"     >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME} 
+  echo "    ssl_certificate_key "  ${!TMP_SSL_KEY} ";"  >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
-  cat /etc/nginx/ssl-template-part2.cfg                 >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  cat /etc/nginx/ssl-template-part2.cfg                 >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
-  echo ""                                               >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "    access_log "  ${!TMP_LOG_FILE} ";"          >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo ""                                               >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo ""                                               >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "    access_log "  ${!TMP_LOG_FILE} ";"          >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo ""                                               >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
-  cat /etc/nginx/ssl-template-part3.cfg                 >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  cat /etc/nginx/ssl-template-part3.cfg                 >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
-  echo "      proxy_pass          http://localhost:"${!TMP_LISTEN_PORT} ";"    >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "      proxy_read_timeout  90;"                                         >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo ""                                                                      >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "      proxy_redirect      http://localhost:"${!TMP_LISTEN_PORT}" https://"${!TMP_DOMAIN_NAME} ";"  >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "    }"                                                                 >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
-  echo "  }"                                                                   >> /etc/nginx/sites-enabled/${!TMP_DOMAIN_NAME}
+  echo "      proxy_pass          http://localhost:"${!TMP_LISTEN_PORT} ";"    >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "      proxy_read_timeout  90;"                                         >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo ""                                                                      >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "      proxy_redirect      http://localhost:"${!TMP_LISTEN_PORT}" https://"${!TMP_DOMAIN_NAME} ";"  >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "    }"                                                                 >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
+  echo "  }"                                                                   >> /etc/nginx/conf.d/${!TMP_DOMAIN_NAME}
   
   echo "Config set up for " ${!TMP_DOMAIN_NAME}
   echo ""
