b3fe6bde9430c50d4892c24d1d7788b38520f238
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-02-08 17:13:24 +0100

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sleep 10
-echo "Install bash and tools necessary later"
-apk add bash bash-doc bash-completion util-linux pciutils usbutils coreutils binutils findutils grep
 /prepare-env.sh && echo "Preparation finished"
 echo " finished preparation"
 exec "$@"
